ece79062286a7acaa8c70223f3f65a67148e27b3
Sof <skrbicsofija@gmail.com>
2021-03-18 19:30:09 +0100

    Mobile design finished

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-function openMenu() {
-  const hamburger = document.getElementById('hamburger')
-  const mobileMenu = document.getElementById('main')
-  hamburger.addEventListener('click', () => {
+const hamburger = document.getElementById('hamburger')
+const mobileMenu = document.getElementById('main')
+hamburger.addEventListener('click', () => {
     if (mobileMenu.style.display === 'none') {
       mobileMenu.style.display = 'block';
+      hamburger.setAttribute('src', './images/icon-close.svg')
     } else {
+      hamburger.setAttribute('src', './images/icon-hamburger.svg')
       mobileMenu.style.display = 'none';
     }
   });
-}
